d2b1ecb08a6bbfec4c91c051df493c584a0b0938
quantumduck <quantum.duck@hotmail.com>
2017-04-07 14:58:27 -0400

    form_tag vs form_for conflict solved. editing CVM done.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,11 +13,11 @@ class PicturesController < ApplicationController
 
   def create
     # make a new picture with what picture_params returns (which is a method we're calling)
-    # @picture = Picture.new(picture_params)
-    @picture = Picture.new  # Alternate method
-    @picture.title = params[:title]
-    @picture.url = params[:url]
-    @picture.artist = params[:artist]
+    @picture = Picture.new(picture_params_alt)
+    # @picture = Picture.new  # Alternate method
+    # @picture.title = params[:title]
+    # @picture.url = params[:url]
+    # @picture.artist = params[:artist]
     if @picture.save
       # if the save for the picture was successful, go to index.html.erb
       redirect_to pictures_url
@@ -31,11 +31,29 @@ class PicturesController < ApplicationController
     #              "Artist: #{params[:artist]}"
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
 private
 
-  # def picture_params
-  #   # params sanitation method
-  #   params.require(:picture).permit(:artist, :title, :url)
-  # end
+  def picture_params
+    # params sanitation method (only works for form_for)
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
+  def picture_params_alt
+    # params sanitation method (only works for form_tag)
+    params.permit(:artist, :title, :url)
+  end
 
 end
