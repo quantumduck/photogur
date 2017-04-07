e4d056bb9e6f0c94ff5bbc705e8f4c5fee03fa4e
quantumduck <quantum.duck@hotmail.com>
2017-04-07 13:12:32 -0400

    seeding database and adding pictures to index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Picture.create(
+  title:  'The old church on the coast of White sea',
+  artist: 'Sergey Ershov',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
+)
+
+Picture.create(
+  title:  'Sea Power',
+  artist: 'Stephen Scullion',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+)
+
+Picture.create(
+  title:  'Into the Poppies',
+  artist: 'John Wilhelm',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+)
