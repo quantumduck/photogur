4d8be39637c4138ab4ccc9373a369f441a662a11
quantumduck <quantum.duck@hotmail.com>
2017-04-12 16:25:39 -0400

    restored comment functionality in pictures_controller.rb

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,6 +5,7 @@ class PicturesController < ApplicationController
 
   def show
     @picture = Picture.find(params[:id])
+    @comments = Comment.where(picture: params[:id]).order(created_at: :DESC)
   end
 
   def new
